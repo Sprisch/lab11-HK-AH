717dd52029f9c6e5fc004952708f59eb3d5d0d44
Sprisch <ashley.house12322@gmail.com>
2026-04-13 18:38:28 -0400

    please work

--- test_calculator.py
+++ test_calculator.py
@@ -2,14 +2,6 @@ import unittest
 from calculator import *
 
 class TestCalculator(unittest.TestCase):
-    ######### Partner 2
-    # def test_add(self): # 3 assertions
-    #     fill in code
-
-    # def test_subtract(self): # 3 assertions
-    #     fill in code
-    # ##########################
-
     ######## Partner 1
     def test_multiply(self): # 3 assertions
         self.assertEqual(multiply(2,3), 6)
@@ -21,22 +13,6 @@ class TestCalculator(unittest.TestCase):
         self.assertEqual(div(20, 10), 2)
         self.assertEqual(div(5, 2), 2.5)
     # ##########################
-
-    ######## Partner 2
-    # def test_divide_by_zero(self): # 1 assertion
-    #     # call division function inside, example:
-    #     # with self.assertRaises(<INSERT_ERROR_TYPE>):
-    #     #     div(0, 5)
-    #     fill in code
-
-    # def test_logarithm(self): # 3 assertions
-    #     fill in code
-
-    # def test_log_invalid_base(self): # 1 assertion
-    #     # use same technique from test_divide_by_zero
-    #     fill in code
-    # ##########################
-    
     ######## Partner 1
     def test_log_invalid_argument(self): # 1 assertion
         # call log function inside, example:
@@ -61,6 +37,31 @@ class TestCalculator(unittest.TestCase):
             square_root(-4)
 
         self.assertEqual(square_root(25), 5)
+
+class TestCalculator(unittest.TestCase):
+    def test_add(self):  # 3 assertions
+        assert add(1, 2) == 3
+        assert add(2, 3) == 5
+        assert add(3, 4) == 7
+
+    def test_subtract(self):  # 3 assertions
+        assert sub(1, 2) == -1
+        assert sub(10, 5) == 5
+        assert sub(-1, -2) == 1
+
+    ######## Partner 2
+    def test_divide_by_zero(self):  # 1 assertion
+        with self.assertRaises(ZeroDivisionError):
+            div(0, 5)
+
+    def test_logarithm(self):  # 3 assertions
+        assert logarithm(1, 2) == 0
+        assert logarithm(7, 49) == 0.5
+        assert logarithm(8, 2) == 3
+
+    def test_log_invalid_base(self):  # 1 assertion
+        with self.assertRaises(ValueError):
+            logarithm(5, 0)
 
 # Do not touch this
 if __name__ == "__main__":
